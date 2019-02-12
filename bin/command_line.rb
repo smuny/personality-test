ecda68051bd3fb5d97375435015e2096f183627e
Cristian Leiva <cleiva94.cl@gmail.com>
2019-02-12 09:29:54 -0500

    something

--- bin/command_line.rb
+++ bin/command_line.rb
@@ -2,7 +2,7 @@ require_relative '../config/environment.rb'
 require_relative '../bin/apps/forest.rb'
 require_relative '../bin/apps/castle.rb'
 
-
+#WE need to seperare the answers to questions and tests. As of now in the console its creating a bunch of content answers. The user can determine its answers that was but i'd like it to be by question or by test. A user specifc question shows its answer.
 class Command
 
   def player(name)
@@ -10,7 +10,19 @@ class Command
     user
   end
 
-  def test_menu(user)
+  def menu
+    prompt = TTY::Prompt.new
+    prompt.select("Choose an option") do |menu|
+      menu.choice 'Take a test' do test_menu end
+      menu.choice 'See your previous tests'
+      menu.choice 'Delete your previous tests'
+    end
+  end
+#########################################################################################################
+#  Test Options                                                                                         #
+#########################################################################################################
+
+  def test_menu(user = nil)
     prompt = TTY::Prompt.new
     prompt.select("Which of the tests would you like to do?") do |menu|
       menu.choice 'The Forest Test.' do forest(user) end
@@ -38,6 +50,7 @@ class Command
     prompt = TTY::Prompt.new
     user_name = prompt.ask("What's your name?")
     current_user = player(user_name)
+    menu
     test_menu(current_user)
   end
 
